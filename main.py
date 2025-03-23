a9fa6f5cd941182b331ad067190b82b74bc13320
MrRipduck <dion_arnold@hotmail.com.au>
2025-03-23 18:32:55 +1000

    added word count in main

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ def get_book_text(filepath):
         return f.read()
         
 def main():
+    word_count = 0
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-
+    
+    words = book_text.split()
+    for count in words:
+        word_count += 1
+    print(f"{word_count} words found in the document")
 main()
+
+
+
+
+
