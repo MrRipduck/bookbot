60c5d12016eb6bb5b31b8f1e0f3eda0d0892c1cb
MrRipduck <dion_arnold@hotmail.com.au>
2025-03-24 19:16:38 +1000

    add count letters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, symbol_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +8,7 @@ book_text = get_book_text("books/frankenstein.txt")
 
 get_num_words(book_text)
 
-
+symbol_count(book_text)
 
 
 
--- stats.py
+++ stats.py
@@ -5,3 +5,17 @@ def get_num_words(text):
     for count in words:
         word_count += 1
     print(f"{word_count} words found in the document")
+
+def symbol_count(text):
+    lower_case_book = str.lower(text)
+    symbols = {}
+    for letter in lower_case_book:
+        if letter in symbols:
+            symbols[letter] += 1
+        else:
+            symbols[letter] = 1
+    print(symbols)
+
+def sorting_letters(dict):
+    
+    
