1771283c5dbe906582264083b86a828492d51dcb
MrRipduck <dion_arnold@hotmail.com.au>
2025-03-26 19:23:27 +1000

    finished project

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import sys 
+
 from stats import get_num_words, symbol_count, create_char_count_list
+
+if len(sys.argv) >= 2:
+    filepath = sys.argv[1]
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
-book_text = get_book_text("books/frankenstein.txt")
+book_text = get_book_text(filepath)
 
 word_count = get_num_words(book_text)
 symbols = symbol_count(book_text)
